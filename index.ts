a2072b23a60e8e1289071a3219109dd2b2ee9c63
Henry Gelderbloem <hgelderbloem@icloud.com>
2024-02-25 14:53:45 +0000

    Fixed TypeError: Cannot read properties of undefined (reading 'token') issue.

--- index.ts
+++ index.ts
@@ -5,10 +5,15 @@ async function main() {
     const password = 'bewbA0-mabmyg-cuktac';
 
     const eufySession = new EufyHomeSession(email, password);
-    eufySession.Initialize(email, password)
+    if (eufySession.email !== null && eufySession.password !== null) {
+        await eufySession.login(eufySession.email, eufySession.password);
+    }
+    //await eufySession.login(eufySession.email, eufySession.password);
+    const userInfo = await eufySession.get_user_info();
+    console.log(userInfo)
 
     let eufyClient = new EufyHomeSession(process.argv[2], process.argv[3]);
-    let userInfo = await eufyClient.get_user_info();
+    //let userInfo = await eufyClient.get_user_info();
 
     let tuyaClient = new TuyaAPISession(`eh-${userInfo["id"]}`, userInfo["phone_code"]);
 
@@ -23,4 +28,6 @@ async function main() {
     }
 }
 
-main();
+main().catch((error) => {
+    console.error(error);
+});
